a30559253d2839d4bd73da2e3d781b10ed1b355b
sth <sth0067@gmail.com>
2023-03-20 17:53:10 -0500

    added all placeholder html - to be polished after js

---
+++ Assets/script.js
@@ -0,0 +1,18 @@
+var searchBtn = $('searchBtn');
+var searchInput = $('searchInput');
+var cityHistory = $('cityHistory');
+
+
+var printHistory = function (name) {
+    var listEl = $('li');
+    var listDetail = name;
+    //do we need the above var?
+    //ON 05-24..
+
+}
+
+var handleSearchSubmit = function (event) {
+    event.preventDefault();
+
+
+}
