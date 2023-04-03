c1e356f72577dfb38bdceaf3922476394897e662
sth <sth0067@gmail.com>
2023-04-03 18:15:48 -0500

    updated html, debugged current weather display

--- Assets/script.js
+++ Assets/script.js
@@ -20,38 +20,38 @@ $('#searchBtn').click( function() {
     console.log('search');
     let city = $('#searchInput').val();
     var APIKey = "0dca0bd477f22fabee6b70094d95fe34"
-    var geoQueryURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
+    let currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey
+    var forecastQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
 
-    fetch(geoQueryURL).then(function(response){
+    fetch(currentWeatherURL).then(function(response) {
         return response.json();
     })
     .then(function(data) {
-        var lat = data[0].lat;
-        var lon = data[0].lon;
-        var forecastQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIKey
-        var currentDayQuery = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIKey
+        console.log(data);
+        let unixTimestamp = data.dt;
+        let date = dayjs.unix(unixTimestamp);
+        let formatDate = date.format('MM-DD-YYYY');
+        console.log(formatDate);
+        let cityName = data.name;
+        let weatherIcon = data.weather[0].icon;
+        let temperature = data.main.temp;
+        let wind = data.wind.speed;
+        let humidity = data.main.humidity;
 
-        fetch(forecastQueryUrl).then(function(response) {
-            return response.json();
+        //clear previous result
+        $('#forecastData').html(``);
+
+        //create current city weather info
+        let currentWeatherDiv = $("<div>").attr("id", "currentWeather").addClass("card flex-md-row mb-4 box-shadow h-md-250").appendTo($("#forecastData"));
+        let cardBodyDiv = $("<div>").addClass("card-body d-flex flex-column align-items-start").appendTo(currentWeatherDiv);
+        let cityDateH3 = $("<h3>").attr("id", "ctyName").addClass("mb-0 pb-3").appendTo(cardBodyDiv);
+        let cityNameSpan = $("<span>").addClass("city-name").text(cityName + " ").appendTo(cityDateH3);
+        let weatherDateSpan = $("<span>").addClass("weather-date").text("(" + formatDate + ")").appendTo(cityDateH3);
+        let weatherIconImg = $("<img>").addClass("weather-icon").attr("src", "http://openweathermap.org/img/wn/" + weatherIcon + ".png").appendTo(cityDateH3);
+        let temperatureP = $("<p>").addClass("temperature").text("Temperature: " + temperature + "°F").appendTo(cardBodyDiv);
+        let windP = $("<p>").addClass("wind").text("Wind Speed: " + wind + " mph").appendTo(cardBodyDiv);
+        let humidityP = $("<p>").addClass("humidity").text("Humidity: " + humidity + "%").appendTo(cardBodyDiv);
         })
-        .then(function(data) {
-            console.log(data);
-        })
-        fetch(currentDayQuery).then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            let unixTimestamp = data.dt;
-            let date = dayjs.unix(unixTimestamp);
-            let formatDate = date.format('YYYY-MM-DD');
-            console.log(formatDate);
-            let cityName = data.name;
-            //create current city weather info
-            let currentWeatherInfo = $(`<div class="card flex-md-row mb-4 box-shadow h-md-250"></div>`);
-            $('#ctyName').text(cityName);
-            console.log(cityName);
-            
-        })
-    })
+      
+        
 })
