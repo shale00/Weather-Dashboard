8108dedb3f097a421c31cebba18080c94e9c9fbf
sth <sth0067@gmail.com>
2023-05-11 16:16:21 -0500

    updated forecast fetch function, added dayjs plugin

--- Assets/script.js
+++ Assets/script.js
@@ -1,178 +1,199 @@
-var searchBtn = $('searchBtn');
-var searchInput = $('#searchInput');
-var cityHistory = $('cityHistory');
+//Element References
+var searchBtn = $("searchBtn");
+var searchInput = $("#searchInput");
+var cityHistory = $("#cityHistory");
+var forecastContainer = $("#forecastContainer");
+
+//Variable array for search history list
 var searchHistory = [];
 
+//Timezone plugin for day.js
+dayjs.extend(window.dayjs_plugin_timezone);
 
+$("#searchBtn").click(function (event) {
+  event.preventDefault();
+  let city = $("#searchInput").val();
+  var APIKey = "0dca0bd477f22fabee6b70094d95fe34";
+  var geoQueryURL =
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
+    city +
+    "&limit=1&appid=" +
+    APIKey;
 
-function getNextDayWeatherIcons(forecastData) {
-    let wIcons = [];
-    let date = new Date();
-  
-    for (let i = 0; i < 5; i++) {
-      date.setDate(date.getDate() + 1);
-      const dateString = date.toISOString().slice(0, 10);
-      const noonObject = forecastData.list.find(item => {
-        const itemDate = new Date(item.dt * 1000);
-        return itemDate.getHours() === 18 && itemDate.toISOString().slice(0, 10) === dateString;
-      });
-      if (noonObject) {
-        const weatherIcon = noonObject.weather.icon;
-        wIcons.push(weatherIcon);
-      } else {
-        wIcons.push(null);
-      }
-    }
-    console.log(forecastData.list[0].weather[0].icon);
-    console.log(forecastData.list[0].weather[0]);
-    console.log(forecastData.list[0]);
-
-
-    return wIcons;
-  }
-
-$('#searchBtn').click( function(event) {
-    event.preventDefault();
-    let city = $('#searchInput').val();
-    var APIKey = "0dca0bd477f22fabee6b70094d95fe34"
-    let currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
-    var forecastQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
-    console.log('click');
-    if (!searchInput.val()) {
-        return;
-    } 
-
-    fetch(currentWeatherURL).then(function(response) {
-        return response.json();
+  fetch(geoQueryURL)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(data) {
-        let unixTimestamp = data.dt;
-        let date = dayjs.unix(unixTimestamp);
-        let formatDate = date.format('MM-DD-YYYY');
-        let cityName = data.name;
-        let weatherIcon = data.weather[0].icon;
-        let temperature = data.main.temp;
-        let wind = data.wind.speed;
-        let humidity = data.main.humidity;
+    .then(function (data) {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      var forecastQueryUrl =
+        "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&units=imperial&appid=" +
+        APIKey;
+      var currentDayQuery =
+        "https://api.openweathermap.org/data/2.5/weather?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&units=imperial&appid=" +
+        APIKey;
 
+      console.log("click");
+      if (!searchInput.val()) {
+        console.log("No Search Input!");
+        return;
+      }
 
-        //create current city weather info
-        let currentWeatherDiv = $("<div>").attr("id", "currentWeather").addClass("card flex-md-row mb-4 box-shadow h-md-250").appendTo($("#weatherData"));
-        let cardBodyDiv = $("<div>").addClass("card-body d-flex flex-column align-items-start").appendTo($("#currentWeather"));
-        let cityH3 = $("<h3>").attr("id", "ctyName").addClass("mb-0 pb-3").appendTo(cardBodyDiv);
-        let cityNameSpan = $("<span>").addClass("city-name").text(cityName + " ").appendTo(cityH3);
-        let weatherDateSpan = $("<span>").addClass("weather-date").text("(" + formatDate + ")").appendTo(cityH3);
-        let weatherIconImg = $("<img>").addClass("weather-icon").attr("src", "http://openweathermap.org/img/wn/" + weatherIcon + ".png").appendTo(cityH3);
-        let temperatureP = $("<p>").text("Temperature: " + temperature + "°F").appendTo(cardBodyDiv);
-        let windP = $("<p>").text("Wind Speed: " + wind + " mph").appendTo(cardBodyDiv);
-        let humidityP = $("<p>").text("Humidity: " + humidity + "%").appendTo(cardBodyDiv);
+      fetch(currentDayQuery)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          let unixTimestamp = data.dt;
+          let date = dayjs.unix(unixTimestamp);
+          let formatDate = date.format("MM-DD-YYYY");
+          let cityName = data.name;
+          let weatherIcon = data.weather[0].icon;
+          let temperature = data.main.temp;
+          let wind = data.wind.speed;
+          let humidity = data.main.humidity;
+
+          //create current city weather info
+          let currentWeatherDiv = $("<div>")
+            .attr("id", "currentWeather")
+            .addClass("card flex-md-row mb-4 box-shadow h-md-250")
+            .appendTo($("#weatherData"));
+          let cardBodyDiv = $("<div>")
+            .addClass("card-body d-flex flex-column align-items-start")
+            .appendTo($("#currentWeather"));
+          let cityH3 = $("<h3>")
+            .attr("id", "ctyName")
+            .addClass("mb-0 pb-3")
+            .appendTo(cardBodyDiv);
+          let cityNameSpan = $("<span>")
+            .addClass("city-name")
+            .text(cityName + " ")
+            .appendTo(cityH3);
+          let weatherDateSpan = $("<span>")
+            .addClass("weather-date")
+            .text("(" + formatDate + ")")
+            .appendTo(cityH3);
+          let weatherIconImg = $("<img>")
+            .addClass("weather-icon")
+            .attr(
+              "src",
+              "http://openweathermap.org/img/wn/" + weatherIcon + ".png"
+            )
+            .appendTo(cityH3);
+          let temperatureP = $("<p>")
+            .text("Temperature: " + temperature + "°F")
+            .appendTo(cardBodyDiv);
+          let windP = $("<p>")
+            .text("Wind Speed: " + wind + " mph")
+            .appendTo(cardBodyDiv);
+          let humidityP = $("<p>")
+            .text("Humidity: " + humidity + "%")
+            .appendTo(cardBodyDiv);
         });
 
-        fetch(forecastQueryUrl).then(function(response) {
-            return response.json();
+      fetch(forecastQueryUrl)
+        .then(function (response) {
+          return response.json();
         })
-        .then(function(data) {
-            console.log(data);
-            const weatherIcons = getNextDayWeatherIcons(data);
-            console.log(weatherIcons);
+        .then(function (data) {
+          var city = data.city.name;
+          var weather = data.list[0];
+          var date = dayjs().format("M/D/YYYY");
+          var tempF = weather.main.temp;
+          var windMph = weather.wind.speed;
+          var humidity = weather.main.humidity;
+          var iconUrl =
+            "https://openweathermap.org/img/w/" +
+            weather.weather[0].icon +
+            ".png";
+          var iconDescription =
+            weather.weather[0].description || weather[0].main;
 
-            const buckets = {
-                dayOne: { temps: [], windSpeeds: [], humidities: [] },
-                dayTwo: { temps: [], windSpeeds: [], humidities: [] },
-                dayThree: { temps: [], windSpeeds: [], humidities: [] },
-                dayFour: { temps: [], windSpeeds: [], humidities: [] },
-                dayFive: { temps: [], windSpeeds: [], humidities: [] },
-              };
-              
-              data.list.forEach((item) => {
-                const date = new Date(item.dt * 1000);
-                const currentDate = new Date();
-                const diffDays = Math.round((date - currentDate) / (1000 * 60 * 60 * 24));
-              
-                if (diffDays >= 1 && diffDays <= 5) {
-                  const dateString = date.toLocaleDateString('en-US', {month: 'numeric', day: 'numeric', year: 'numeric'});
-                  
-              
-                  if (diffDays === 1) {
-                    buckets.dayOne.temps.push(item.main.temp);
-                    buckets.dayOne.windSpeeds.push(item.wind.speed);
-                    buckets.dayOne.humidities.push(item.main.humidity);
-                  } else if (diffDays === 2) {
-                    buckets.dayTwo.temps.push(item.main.temp);
-                    buckets.dayTwo.windSpeeds.push(item.wind.speed);
-                    buckets.dayTwo.humidities.push(item.main.humidity);
-                  } else if (diffDays === 3) {
-                    buckets.dayThree.temps.push(item.main.temp);
-                    buckets.dayThree.windSpeeds.push(item.wind.speed);
-                    buckets.dayThree.humidities.push(item.main.humidity);
-                  } else if (diffDays === 4) {
-                    buckets.dayFour.temps.push(item.main.temp);
-                    buckets.dayFour.windSpeeds.push(item.wind.speed);
-                    buckets.dayFour.humidities.push(item.main.humidity);
-                  } else if (diffDays === 5) {
-                    buckets.dayFive.temps.push(item.main.temp);
-                    buckets.dayFive.windSpeeds.push(item.wind.speed);
-                    buckets.dayFive.humidities.push(item.main.humidity);
-                  }
+          var card = $("<div>").addClass("card");
+          var cardBody = $("<div>").addClass("card-body");
+          var heading = $("<h2>")
+            .addClass("h3 card-title")
+            .text(city + " (" + date + ")");
+          var weatherIcon = $("<img>")
+            .addClass("weather-img")
+            .attr("src", iconUrl)
+            .attr("alt", iconDescription);
+          var tempEl = $("<p>")
+            .addClass("card-text")
+            .text("Temp: " + tempF + "°F");
+          var windEl = $("<p>")
+            .addClass("card-text")
+            .text("Wind: " + windMph + " MPH");
+          var humidityEl = $("<p>")
+            .addClass("card-text")
+            .text("Humidity: " + humidity + " %");
 
+          cardBody.append;
 
+          // Unix timestamps
+          var startDate = dayjs().add(1, "day").startOf("day").unix();
+          var endDate = dayjs().add(6, "day").startOf("day").unix();
 
-                };            
-              });
-              
+          var headingCol = $("<div>").addClass("col-12");
+          var heading = $("<h4>").text("5-Day Forecast:");
+          headingCol.append(heading);
 
-              const averages = {};
-                Object.entries(buckets).forEach(([day, values]) => {
-                    const avgTemp = values.temps.reduce((acc, curr) => acc + curr) / values.temps.length;
-                    const avgWind = values.windSpeeds.reduce((acc, curr) => acc + curr) / values.windSpeeds.length;
-                    const avgHumidity = values.humidities.reduce((acc, curr) => acc + curr) / values.humidities.length;
-                    averages[day] = { avgTemp, avgWind, avgHumidity, noonObject: values.noonObject };
-                })
+          forecastContainer.empty();
+          forecastContainer.append(headingCol);
 
-
-                // Get a reference to the forecast container element
-                const forecastContainer = $('#forecastContainer');
-
-                // Loop through each day in the averages object and create a card for it
-                Object.entries(averages).forEach(([day, values], index) => {
-
-                const date = new Date();
-                date.setDate(date.getDate() + index + 1);
-                const dateString = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-
-                const noonObject = data.list.find(item => {
-                    const itemDate = new Date(item.dt * 1000);
-                    return itemDate.getHours() === 12 && itemDate.toDateString() === date.toDateString();
+          data.list.forEach(function (item) {
+            // Only retrieves data 5 days after today's date
+            if (item.dt >= startDate && item.dt < endDate) {
+              // Returns data for 12 o'clock (noon)
+              if (item.dt_txt.slice(11, 13) === "12") {
+                var cardDeck = $("<div>").addClass(
+                  "card-deck mb-5 text-center"
+                );
+                var container = $("<div>").addClass("container");
+                var row = $("<div>").addClass("row");
+                var colMd2 = $("<div>").addClass("col-md-2");
+                var card = $("<div>").addClass("card");
+                var cardBody = $("<div>").addClass("card-body");
+                var date = $("<p>")
+                  .addClass("card-text")
+                  .text(dayjs(item.dt_txt).format("M/D/YYYY"));
+                var iconUrl = `https://openweathermap.org/img/w/${item.weather[0].icon}.png`;
+                var iconDescription =
+                  item.weather[0].description || item.weather[0].main;
+                var weatherIcon = $("<img>").attr({
+                  src: iconUrl,
+                  alt: iconDescription,
+                  class: "weather-img",
                 });
-             
-                console.log(noonObject.weather);
-                // const weatherIcon = noonObject.weather[0].icon;
+                var temp = $("<p>")
+                  .addClass("card-text")
+                  .text(`Temp: ${item.main.temp}°F`);
+                var wind = $("<p>")
+                  .addClass("card-text")
+                  .text(`Wind: ${item.wind.speed} MPH`);
+                var humidity = $("<p>")
+                  .addClass("card-text")
+                  .text(`Humidity: ${item.main.humidity} %`);
 
-                //add 5day divs
-
-                //NEED TO MAKE SURE ONLY ONE COL-MD-2
-                const card = document.createElement('div');
-                card.classList.add('col-md-2');
-                card.innerHTML = `
-                    <div class="card">
-                    <div class="card-body">
-                        <p class="card-text">${dateString}</p>
-                        
-                        <p class="card-text">${values.avgTemp.toFixed(1)} °F</p>
-                        <p class="card-text">${values.avgWind.toFixed(1)} mph</p>
-                        <p class="card-text">${values.avgHumidity.toFixed(0)}%</p>
-                    </div>
-                    </div>`;
-                // Append the card to the forecast container
-                  forecastContainer.append(card);
-                });
-
-
-            });
-         })
-
-        //  <img class="weather-icon" src="http://openweathermap.org/img/wn/${weatherIcon}.png">
-
-
-        
-          
+                cardBody.append(date, weatherIcon, temp, wind, humidity);
+                card.append(cardBody);
+                colMd2.append(card);
+                row.append(colMd2);
+                container.append(row);
+                cardDeck.append(container);
+                forecastContainer.append(cardDeck);
+              }
+            }
+          });
+        });
+    });
+});
